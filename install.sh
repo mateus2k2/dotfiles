eb4890beed969c3ef0da10b33068856327bade9b
Mateus <mateus.moreira@aluno.ufop.edu.br>
2024-07-16 10:34:31 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -41,5 +41,5 @@ cp ./.zshrc ~
 ########################################################################################################################
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >> ~/zshrc
-wget https://raw.githubusercontent.com/mateus2k2/dotfiles/main/.p10k.zsh -O ~/p10k.zsh
+echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >> ~/.zshrc
+wget https://raw.githubusercontent.com/mateus2k2/dotfiles/main/.p10k.zsh -O ~/.p10k.zsh
