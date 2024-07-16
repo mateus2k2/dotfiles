dee8c7a1fc09d2a69f4abb790f17abfdb6a3739c
Mateus <mateus.moreira@aluno.ufop.edu.br>
2024-07-16 10:49:07 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -42,6 +42,7 @@ cp ./.zshrc ~
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 wget https://raw.githubusercontent.com/mateus2k2/dotfiles/main/.p10k.zsh -O ~/.p10k.zsh
-source ~/powerlevel10k/powerlevel10k.zsh-theme
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >>~/.zshrc
 
